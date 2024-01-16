7cb146a9723c5b28117eee5a8d85aad2179fe2ce
areshaistg <areshaistg@gmail.com>
2024-01-17 05:52:54 +0800

    fix(wm): use experimental backend

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -40,4 +40,4 @@ awful.screen.connect_for_each_screen(function(s)
     awful.tag({"一", "二", "三", "四", "五", "六", "七", "八", "九"}, s, awful.layout.layouts[1])
 end)
 
-awful.spawn("picom -b")
+awful.spawn("picom -b --experimental-backend")
